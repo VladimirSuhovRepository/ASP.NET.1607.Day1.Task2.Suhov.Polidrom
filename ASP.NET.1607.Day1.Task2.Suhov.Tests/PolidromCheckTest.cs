bbdf511a18fb7846d2619e3992bfc6cd31f57e57
vladimir_suhov <vladimir_suhov@outlook.com>
2016-07-12 03:03:00 -0700

    initial

---
+++ ASP.NET.1607.Day1.Task2.Suhov.Tests/PolidromCheckTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ASP.NET._1607.Day1.Task2.Suhov.Tests
+{
+    [TestClass]
+    public class PolidromCheckTest
+    {
+        [TestMethod]
+        public void TestIsPolidromPolidromValues()
+        {
+            // Arrange
+            var arr = new List<int>() { 123321, -202, 9889, -5555, 8 };
+            // Act
+            // Assert
+            arr.ForEach(delegate (int numb) {
+                Assert.IsTrue(PolidromCheck.IsPolidrom(numb));
+            });
+        }
+        [TestMethod]
+        public void TestIsPolidromNotPolidromValues()
+        {
+            // Arrange
+            var arr = new List<int>() { 1233212, -2022, 988933, -553556, 84 };
+            // Act
+            // Assert
+            arr.ForEach(delegate (int numb) {
+                Assert.IsFalse(PolidromCheck.IsPolidrom(numb));
+            });
+        }
+
+    }
+}
---
+++ ASP.NET.1607.Day1.Task2.Suhov/PolidromCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET._1607.Day1.Task2.Suhov
+{
+    public static class PolidromCheck
+    {
+        /// <summary>Checking whether number is polidrom</summary> 
+        /// <param name="value">Number which should be checked</param> 
+        /// <returns>If the number <paramref name="n"/> is polidrom, result:(<paramref name="value"/>) will be true</returns> 
+        public static bool IsPolidrom(int numb)
+        {
+            string str = Math.Abs(numb).ToString();
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                for (int ii = str.Length - 1-i; ii > 0; ii--)
+                {
+                    if (str[i] != str[ii])
+                        return false;
+                    else
+                        break;
+                }
+            }
+            return true;
+        }
+    }
+}
